68a71f63c6f66469fe14b64f70a2ba5b94c48c13
Petr Hrehorovsky <washi@volny.cz>
2022-04-18 16:28:09 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # origam-ftp
-Origam workflow service agent for FTP access.
+**Origam** workflow service agent for FTP access.
+
+Supported **Origam** versions: 2022.1+.
+
+## How to use
+Reference `origam-ftp` package in your **Origam** project, add the compiled dlls to the server instance folder and adjust 
+ `Origam.ServerCore.deps.json`.
+```json
+# add this to targets
+"FluentFTP/32.3.1": {
+    "dependencies": {
+      "System.Console": "4.3.0",
+      "System.IO": "4.3.0",
+      "System.Net.NameResolution": "4.3.0",
+      "System.Net.Security": "4.3.0",
+      "System.Net.Sockets": "4.3.0",
+      "System.Runtime": "4.3.0",
+      "System.Threading.Tasks": "4.3.0",
+      "System.Threading.Thread": "4.3.0"
+    },
+    "runtime": {
+      "lib/netstandard2.0/FluentFTP.dll": {
+        "assemblyVersion": "32.3.1.0",
+        "fileVersion": "32.3.1.0"
+      }
+    },
+    "compile": {
+      "lib/netstandard2.0/FluentFTP.dll": {}
+    }
+  }
+```
+```json
+# add this to libraries
+"FluentFTP/32.3.1": {
+  "type": "package",
+  "serviceable": true,
+  "sha512": "sha512-Worl3dtG1Kw7gHlNGIUZhJiEPZ5LytLhrMtdyBwlY0foR6/nRJg2dV5ZdFBLw0CNwQ2RxLlcrqnaiFDnG/wr2g==",
+  "path": "fluentftp/32.3.1",
+  "hashPath": "fluentftp.32.3.1.nupkg.sha512"
+}
+```
---
+++ src/Origam.Ftp/Origam.Ftp/FtpServiceAgent.cs
@@ -0,0 +1,96 @@
+﻿#region license
+/*
+Copyright 2005 - 2020 Advantage Solutions, s. r. o.
+
+This file is part of ORIGAM (http://www.origam.org).
+
+ORIGAM is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ORIGAM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ORIGAM. If not, see <http://www.gnu.org/licenses/>.
+*/
+#endregion
+
+using System.Collections;
+using FluentFTP;
+using log4net;
+using Origam.Service.Core;
+
+namespace Origam.Ftp;
+
+public class FtpServiceAgent : IExternalServiceAgent
+{
+    private static readonly ILog log = 
+        LogManager.GetLogger(typeof(FtpServiceAgent));
+
+    public object Result { get; private set; } = null!;
+    public Hashtable Parameters { get; } = new();
+    public string MethodName { get; set; } = null!;
+    public string TransactionId { get; set; } = null!;
+
+    public void Run()
+    {
+        var useSecuredConnection = true;
+        if(Parameters["Host"] is not string)
+        {
+            throw new InvalidCastException("Host is not string.");
+        }
+        if(Parameters["Username"] is not string)
+        {
+            throw new InvalidCastException("Username is not string.");
+        }
+        if(Parameters["Password"] is not string)
+        {
+            throw new InvalidCastException("Password is not string.");
+        }
+        if(Parameters["Path"] is not string)
+        {
+            throw new InvalidCastException("Path is not string.");
+        }
+        if(Parameters["UseSecuredConnection"] is bool useSecuredConnectionParam)
+        {
+            useSecuredConnection = useSecuredConnectionParam;
+        }
+        Result = MethodName switch
+        {
+            "DownloadFile" => DownloadFile(
+                (string)Parameters["Host"]!,
+                useSecuredConnection,
+                (string)Parameters["Username"]!, 
+                (string)Parameters["Password"]!,
+                (string)Parameters["Path"]!),
+            _ => throw new ArgumentOutOfRangeException(@"MethodName",
+                MethodName)
+        };
+    }
+    private static object DownloadFile(
+        string host, 
+        bool useSecuredConnection,
+        string username, 
+        string password, 
+        string path)
+    {
+        if(log.IsDebugEnabled)
+        {
+            log.DebugFormat(
+                "Downloading {0} from {1}.", path, host);
+        }
+        using var ftp = new FtpClient(host, username, password);
+        using var output = new MemoryStream();
+        if(useSecuredConnection)
+        {
+            ftp.EncryptionMode = FtpEncryptionMode.Explicit;
+        }
+        ftp.Connect();
+        ftp.Download(output, path);
+        return output.ToArray();
+    }
+}
